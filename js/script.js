660374c457e6b8e8f84832effd0f01410dc81f2d
Sam Fernandes <samwallflowergirl@gmail.com>
2021-05-20 23:13:15 -0300

    retirei uns códigos aleatórios

--- js/script.js
+++ js/script.js
@@ -17,35 +17,3 @@ createUsernames(accounts);
 // ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
-
-// const dogsKate = [4,1,15,8,3];
-// const dogsJulia = [3,5,2,12,7];
-
-// function checkDogs(julias, kates) {
-//   const dogs = [...julias.slice(1, -2), ...kates];
-
-//   dogs.forEach( (dog, i) => {
-//   dog >= 3
-//       ? console.log(`Dog number ${i+1} is an adult and its ${dog} years old.`)
-//       : console.log(`Dog number ${i+1} is still a puppy.`);
-//   })
-// }
-
-// function dogsToHumanAge (age) {
-//   return age <= 2 ? 2 * age : 16 + age * 4;
-// }
-
-// function calcAverageHumanAge (ages) {
-//   const humanAge = ages
-//     .map(dogsToHumanAge)
-//     .filter(age => age >= 18);
-
-//   return humanAge.reduce((acc, age) => acc + age, 0) / humanAge.length;
-// }
-
-// checkDogs(dogsJulia, dogsKate);
-// console.log(calcAverageHumanAge (dogsJulia));
-// console.log(calcAverageHumanAge (dogsKate));
-// console.log(calcAverageHumanAge ([5,2,4,1,15,8,3]));
